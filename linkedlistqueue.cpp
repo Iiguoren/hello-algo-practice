a0a820e1a9f541cab43089e8b811c2592f392293
Iiguoren <1329314711@qq.com>
2024-07-04 16:55:17 +0800

    1

---
+++ linkedlistqueue.cpp
@@ -0,0 +1,75 @@
+#include <iostream>
+#include "listnode.h"
+
+class linkedlistqueue{
+    private:
+    listnode *front;
+    listnode *rear;
+    int queuesize;
+    
+    public:
+    linkedlistqueue(){
+    front=nullptr;
+     rear=nullptr; 
+     queuesize=0;
+     }
+
+    int size(){
+        return queuesize;
+    }
+
+    bool isempty(){
+        return (queuesize==0);
+    }
+
+    void push(int num){
+        listnode *node = new listnode(num);
+        if(front == nullptr){
+            rear = node;
+            front = node;
+            }
+        else{
+        rear->next = node;  
+        rear = node;} 
+        queuesize++;
+    }
+
+    int peek(){
+        if (queuesize == 0 )
+            return  -1;
+        else{
+            return front->value;
+        }
+    }
+
+    int pop(){
+        int num = peek();
+        listnode *tmp = front;
+        front = front->next;
+        delete tmp;
+        queuesize--;
+        return num;
+    }
+
+    void printqueue(){
+        listnode *tmp = front;
+        while(tmp != nullptr)
+        {
+            std::cout<<tmp->value;
+            tmp = tmp->next;
+        }
+        std::cout<<std::endl;
+    }
+};
+
+int main(){
+    linkedlistqueue queue;
+    queue.push(0);
+    queue.push(1);
+    queue.push(2);
+    queue.push(3);
+    queue.printqueue();
+    int num  = queue.pop();
+    std::cout<<num<<std::endl;
+    queue.printqueue();
+}
---
+++ listnode.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+
+struct listnode{
+    int value;
+    listnode *next;
+
+    listnode(int num) : value(num), next(nullptr) {};
+    ~listnode(){};
+};
+
+void insert(listnode *n0, listnode *p)
+{
+    listnode *n1 = n0->next;
+    n0->next = p;
+    p->next = n1;
+}
+
+void printlistnode(listnode *head)
+{
+    listnode *current = head;
+    while(current != nullptr)
+    {
+        std::cout<<current->value<<"";
+        current = current->next;
+    }
+    std::cout<<std::endl;
+}
+
+listnode *find(listnode *n0, int val)
+{
+
+     while (n0 != nullptr)
+     {
+        if(n0->value == val)
+            return n0;
+        else
+            n0 = n0->next;
+     }
+
+}
+
+int main()
+{
+    listnode *n0 = new listnode(0);
+    listnode *n1 = new listnode(1);
+    listnode *n2 = new listnode(2);
+    listnode *n3 = new listnode(3);
+    listnode *n4 = new listnode(4);
+    n0->next = n1;
+    n1->next = n2;
+    n2->next = n3;
+    n3->next = n4;
+    printlistnode(n0);
+
+    listnode *p = new listnode(0);
+    insert(n0, p);
+
+    printlistnode(n0);
+
+    listnode *node = find(n0, 3);
+    std::cout<<node->value<<std::endl;
+}
